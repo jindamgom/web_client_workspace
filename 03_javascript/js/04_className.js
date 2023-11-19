38e45570e8e1e85bc87d8d53d0b79f4a10098138
jindamgom <update_set@naver.com>
2023-11-19 23:51:48 +0900

    1119 : 구매정보 실습과제 완료 및 17일 수업 내용 메모 추가.

--- 03_javascript/js/04_className.js
+++ 03_javascript/js/04_className.js
@@ -6,6 +6,8 @@
  *      -remove(className:string)
  *      -toggle(className:string)
  */
+
+
 function checkSubject(checkbox)
 {
     //log는 콤마로 연결해서 찍을 수 있다
@@ -15,26 +17,26 @@ function checkSubject(checkbox)
     console.log(td);
     //.on 토글(toggle) 
     //1.삼항연산자
-    td.className = checkbox.checked?"on":"";
+    //td.className = checkbox.checked?"on":"";
 
     //2.if-else문
-    if(checkbox.checked)
-    {
-        td.className="on"; 
-    }
-    else{
-        td.className="";
-    }
+    // if(checkbox.checked)
+    // {
+    //     td.className="on"; 
+    // }
+    // else{
+    //     td.className="";
+    // }
 
 
     //3.
-    if(checkbox.checked)
-    {
-       td.classList.add("on");
-    }
-    else{
-        td.classList.remove("on");
-    }
+    // if(checkbox.checked)
+    // {
+    //    td.classList.add("on");
+    // }
+    // else{
+    //     td.classList.remove("on");
+    // }
     
     //4.
     td.classList.toggle("on");
